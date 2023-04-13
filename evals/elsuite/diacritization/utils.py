23cc12f826f8eec1fed18b730b2f5a98160f92a7
Zaid Alyafeai <alyafey22@gmail.com>
2023-04-13 05:45:33 +0000

    match diacritics

--- evals/elsuite/diacritization/utils.py
+++ evals/elsuite/diacritization/utils.py
@@ -1,5 +1,35 @@
 from diacritization_evaluation import wer, der
 import re
+from pyarabic.araby import strip_diacritics
+import difflib
+
+def match_diacritics(p_, e_):
+
+    p = strip_diacritics(p_).split(" ")
+    e = strip_diacritics(e_).split(" ")
+
+    p_ = p_.split(" ")
+    e_ = e_.split(" ")
+    
+
+    # initiate the Differ object
+    d = difflib.Differ()
+
+    diff = d.compare(e, p)
+    matching = list(a for a in diff)
+    out = []
+    i = 0
+    for a in matching:
+        # print(a)
+        if a.startswith("-"):
+            out.append(e[i])
+        elif a.startswith("+"):
+            i += 1
+        elif a.startswith(" "):
+            out.append(p_[i])
+            i += 1
+    assert len(out) == len(e_) 
+    return " ".join(e_), " ".join(out).strip()
 
 def post_process(txt):
   puncts = '؟،-[]:؛;()$#@&+=_-{}"\'.\n/\\0123456789١٢٣٤٥٦٧٨٩٠١'
@@ -11,10 +41,13 @@ def post_process(txt):
     out += c
   return re.sub(' +', ' ', out).strip()
 
-
 def calculate_diacritization_score(predicted, expected):
     # remove punctuations and special characters
     predicted = post_process(predicted)
+
+    # pinalize words with typos by replacing them with original words
+    predicted, expected = match_diacritics(predicted, expected)
+    # print("finished matching")
 
     der_ = der.calculate_der(
         expected,
